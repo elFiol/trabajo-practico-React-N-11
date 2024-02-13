0a9dd31ca04df89e6b1a20373ed17c45d6808474
Fiol Thiago <fiolthiago9@gmail.com>
2024-02-13 10:36:26 -0300

    añadimos la funcion para mostrar el spinner cuando esta haciendo la peticion y ademas agregamos la funcion para hacer la peticion, tambien una funcion para ver si hay elementos mostrar si hay o no resultados asi dibujar las card con breve info

--- src/components/FormNoticia.jsx
+++ src/components/FormNoticia.jsx
@@ -1,14 +1,38 @@
 import { Button, Container, Form } from "react-bootstrap";
+import Spinner from 'react-bootstrap/Spinner';
 import ListaNoticias from "./ListaNoticias";
+import { useState } from "react";
 
 const FormNoticia = () => {
+    const [noticias, setNoticias] = useState([])
+    const [mostrarSpinner, setMostrarSpinner] = useState(false)
+    const [categoriaInput, setCategoriaInput] = useState("")
+
+    const API_KEY = "&apiKey=e2d1b266844542de83b64e2878769d6c"
+    const API = "https://newsapi.org/v2/top-headlines?q=Apple&from=2024-02-12&sortBy=popularity&category="
+
+    const hacerPeticiion = async () => {
+        setMostrarSpinner(true)
+        const peticion = await fetch(API + categoriaInput + API_KEY)
+        const datos = await peticion.json()
+        setNoticias(datos.articles)
+        setMostrarSpinner(false)
+    }
+
+    const handlerSubmit = (e) => {
+        e.preventDefault()
+        hacerPeticiion()
+    }
+
+    const mostrarComponente = mostrarSpinner ? (
+        <div className="text-center my-3"><Spinner animation="border" role="status"><span className="visually-hidden">Loading...</span></Spinner></div>) : (<ListaNoticias noticias={noticias}></ListaNoticias>)
     return (
         <Container className="my-4">
             <div className="d-lg-flex w-100 border">
                 <p className="mx-3 my-4">Buscar por categoria:</p>
-                <Form className="w-100 my-4 px-3">
+                <Form className="w-100 my-4 px-3" onSubmit={handlerSubmit}>
                     <Form.Group>
-                        <Form.Select>
+                        <Form.Select onChange={(e) => { setCategoriaInput(e.target.value) }}>
                             <option value="business">Negocios</option>
                             <option value="entertainment">Entretenimiento</option>
                             <option value="general">General</option>
@@ -21,7 +45,7 @@ const FormNoticia = () => {
                     <Button className="mt-4" type="submit">Buscar</Button>
                 </Form>
             </div>
-            <ListaNoticias></ListaNoticias>
+            {mostrarComponente}
         </Container>
     );
 };
--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -1,20 +1,14 @@
 import { Col, Row } from "react-bootstrap";
 import Noticia from "./Noticia";
 
-const ListaNoticias = () => {
-    return (
-        <Row className="my-3">
-            <Col sm={12} md={6} lg={4}>
-                <Noticia></Noticia>
-            </Col>
-            <Col sm={12} md={6} lg={4}>
-                <Noticia></Noticia>
-            </Col>
-            <Col sm={12} md={6} lg={4}>
-                <Noticia></Noticia>
-            </Col>
-        </Row>
-    );
+const ListaNoticias = ({ noticias }) => {
+    const mostrarNoticia = noticias.length === 0 ? (<p className="text-center my-3">No se encontraron resultados</p>) :
+        (<Row>
+            {
+                noticias.map((noticia, posicion) => <Col sm={12} md={6} lg={4} key={posicion}><Noticia noticia={noticia}></Noticia></Col>)
+            }
+        </Row>);
+    return mostrarNoticia;
 };
 
 export default ListaNoticias;
